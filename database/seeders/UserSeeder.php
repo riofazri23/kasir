78cb8980f9f26520fd0cc170278eea5f366a919a
Rio Fazri Septian <rfazri7@gmail.com>
2024-01-03 16:05:50 +0700

    layouting

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,16 +14,18 @@ use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        DB::table('users')->insert([
-            'name'      => 'admin',
+       $data = [
+            ['name'      => 'admin',
             'email'     => 'admin@gmail.com',
             'password'  => Hash::make('admin'),
-            'role'      => 'admin',
-        ]);
+            'role'      => 'admin',],
+            ['name'      => 'kasir',
+            'email'     => 'kasir@gmail.com',
+            'password'  => Hash::make('kasir'),
+            'role'      => 'kasir',],
+        ];
+        User::insert($data);
     }
 }
